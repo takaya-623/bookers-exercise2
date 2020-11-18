33a0d30dd77e428207421deb07aeb71fc6a295f2
EC2 Default User <ec2-user@ip-172-31-90-180.ec2.internal>
2020-11-18 10:13:35 +0000

    [add]マイページ

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -7,17 +7,30 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @Book = Book.new
   end
   
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    # redirect_to books_path(params[:id])
+    redirect_to book_path(@book)
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
   
   private
---
+++ bookers2/app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books.page(params[:page]).reverse_order
+    @book = Book.new
+  end
+end
