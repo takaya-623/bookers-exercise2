7ba5aa27653e74b4c29fcc4708c155178cbb1c62
EC2 Default User <ec2-user@ip-172-31-90-180.ec2.internal>
2020-11-20 02:06:16 +0000

    [add]エラーメッセージ追加

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -14,18 +14,26 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book), notice: 'Book was successfully created.'
+    if @book.save
+      redirect_to book_path(@book), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
+  
 
   def edit
     @book = Book.find(params[:id])
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book), notice: 'Book was successfully updated.'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
   
-  validates :name, uniqueness: true, length: { in: 1..20 }
+  validates :name, uniqueness: true, length: { in: 2..20 }
   validates :body, length: { maximum: 50 }
 end
 
