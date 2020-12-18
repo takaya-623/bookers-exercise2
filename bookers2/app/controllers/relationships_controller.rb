c6d05ec4c7c2849855eae8db66cb64361c37e40d
takaya-623 <takaya.suzuki.0623@gmail.com>
2020-12-18 10:32:06 +0000

    finish task4

--- bookers2/app/controllers/relationships_controller.rb
+++ bookers2/app/controllers/relationships_controller.rb
@@ -1,12 +1,24 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   
   def create
-    Relationship.create(followed_id: params[:user_id])
-    redirect_to request.referrer || root_path
+    current_user.active_relationships.create(followed_id: params[:user_id])
+    redirect_to request.referer || root_path
   end
   
   def destroy
-    current_user.active_relationships.find_by(follower_id: params[:user_id]).destroy
-    redirect_to request.referrer || root_path
+    current_user.active_relationships.find_by(followed_id: params[:user_id]).destroy
+    redirect_to request.referer || root_path
+  end
+  
+  def followings
+    user = User.find(params[:user_id])
+    @followings = user.following
+  end
+  
+  def followers
+    user = User.find(params[:user_id])
+    @followers = user.followers
   end
 end
+
--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "followed_id",dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed
-  
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id",dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
+  
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   
   attachment :profile_image
   
@@ -22,6 +22,7 @@ class User < ApplicationRecord
   def following?(user)
     active_relationships.find_by(followed_id: user.id)
   end
+  
   
 end
 
