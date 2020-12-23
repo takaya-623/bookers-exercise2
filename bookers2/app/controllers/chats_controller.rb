63e52b51173dc4fc41f3723e5f5c407ccebc61bc
takaya-623 <takaya.suzuki.0623@gmail.com>
2020-12-23 10:10:05 +0000

    edit

--- bookers2/app/controllers/chats_controller.rb
+++ bookers2/app/controllers/chats_controller.rb
@@ -21,12 +21,15 @@ class ChatsController < ApplicationController
   
   def create
     @chat = current_user.chats.new(chat_params)
+    # @image = Chat.find(params[:id]).user.profile_image_id
+    binding.pry
     @chat.save
+    # @image.save
   end
   
    
   private
   def chat_params
-    params.require(:chat).permit(:message, :room_id)
+    params.require(:chat).permit(:message, :room_id, :user_id)
   end
 end
